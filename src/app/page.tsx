6af5a2ee525e1c2c738018bfa5a32824ca166a92
Michel Muniagiseny <mafri.michel@gmail.com>
2024-05-21 15:17:43 +0300

    Created feature section and loader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,70 @@
+import { FeatureSection } from "@/components/custom/FeatureSection";
 import HeroSection from "@/components/custom/HeroSection";
-import { flattenAttributes } from "@/lib/utils";
-import qs from "qs";
+import { getHomePageData } from "@/data/loaders";
+// import { flattenAttributes } from "@/lib/utils";
+// import { getStrapiURL } from "@/lib/utils";
+// import qs from "qs";
 
-const homePageQuery = qs.stringify({
-  populate: {
-    blocks: {
-      populate: {
-        image: {
-          fields: ["url", "alternativeText"]
-        },
-        link: {
-          populate: true
-        }
-      }
-    }
-  }
-});
+// const homePageQuery = qs.stringify({
+//   populate: {
+//     blocks: {
+//       populate: {
+//         image: {
+//           fields: ["url", "alternativeText"]
+//         },
+//         link: {
+//           populate: true
+//         },
+//         feature: {
+//           populate: true
+//         }
+//       }
+//     }
+//   }
+// });
 
-async function getStrapiData(path: string) {
-  const baseUrl = "http://localhost:1337";
-
-  const url = new URL(path, baseUrl);
-  url.search = homePageQuery;
-
-  console.log(url.href);
-
-  try {
-    const response = await fetch(url.href, { cache: 'no-store' });
-    const data = await response.json();
-    const flattenedData = flattenAttributes(data);
-    console.dir(flattenedData, { depth: null });
-    return flattenedData;
-  } catch (error) {
-    console.error(error);
+function blockRenderer(block: any) {
+  switch (block.__component) {
+    case "layout.hero-section":
+      return <HeroSection key={block.id} data={block} />;
+    case "layout.features":
+      return <FeatureSection key={block.id} data={block} />;
+    default:
+      return null;
   }
 }
 
+// async function getStrapiData(path: string) {
+//   const baseUrl = getStrapiURL();
+
+//   const url = new URL(path, baseUrl);
+//   url.search = homePageQuery;
+
+//   console.log(url.href);
+
+//   try {
+//     const response = await fetch(url.href, { cache: 'no-store' });
+//     const data = await response.json();
+//     const flattenedData = flattenAttributes(data);
+//     console.dir(flattenedData, { depth: null });
+//     return flattenedData;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
+  const strapiData = await getHomePageData();
 
   const { blocks } = strapiData;
+  if (!blocks) return <div>No blocks found</div>
+
 
   return (
     <main>
-      <HeroSection data={blocks[0]} />
+      {
+        blocks.map((block: any) => blockRenderer(block))
+      }
     </main>
   );
 }
