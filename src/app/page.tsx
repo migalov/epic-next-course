1eb80d3b7c41f9c357d63e8f1e2a7883c9348da4
Michel Muniagiseny <mafri.michel@gmail.com>
2024-05-21 14:01:23 +0300

    02-lesson-fetch-data-in-next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,49 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import HeroSection from "@/components/custom/HeroSection";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"]
+        },
+        link: {
+          populate: true
+        }
+      }
+    }
+  }
+});
 
 async function getStrapiData(path: string) {
-  const baseUrl = 'http:/localhost:1337';
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href);
+
   try {
-    const response = await fetch( baseUrl + path );
+    const response = await fetch(url.href, { cache: 'no-store' });
     const data = await response.json();
-    return data
-  }
-  catch (error) {
-    console.log(error);
-    
+    const flattenedData = flattenAttributes(data);
+    console.dir(flattenedData, { depth: null });
+    return flattenedData;
+  } catch (error) {
+    console.error(error);
   }
 }
 
 export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
 
-  const strapiData = await getStrapiData("/api/home-page/");
-  console.log(strapiData);
-  const {title, description} = strapiData?.data?.attributes;
-  
+  const { blocks } = strapiData;
 
   return (
-    <>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <Button>Button</Button>
-    </>
-  )
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
 }
